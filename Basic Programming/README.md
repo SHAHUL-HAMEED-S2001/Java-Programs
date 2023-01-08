fdcb541aa5cf6d77ba5e78ded13a2cc4dd7ea7ce
SHAHUL HAMEED S <shahulhameed.s101@gmail.com>
2023-01-08 17:39:14 +0530

    upd

---
+++ Basic Programming/README.md
@@ -0,0 +1,4 @@
+# Basic Programs
+
+1. Java Program to Check if a Given Integer is Odd or Even.
+2. 
---
+++ Basic Programming/Solution_1.java
@@ -0,0 +1,17 @@
+// Java Program to Check if a Given Integer is Odd or Even.
+
+import java.util.Scanner;
+
+class Solution_1{
+    public static void main(String[] args) {
+        System.out.println("Java Program to Check if a Given Integer is Odd or Even");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a Number: ");
+        int num = sc.nextInt();
+        sc.close();
+        if (num%2==0)
+            System.out.println("Is Even");
+        else
+            System.out.println("Is Odd");     
+    }
+}
