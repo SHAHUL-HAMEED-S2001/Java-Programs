9d689a524359d01020f2b6e7a49823f3dbed095b
SHAHUL HAMEED S <shahulhameed.s101@gmail.com>
2023-03-09 13:35:36 +0530

    upd

--- Pattern Programs/Solution_1.java
+++ Pattern Programs/Solution_1.java
@@ -1,4 +1,5 @@
 /*
+
    *
   ***
  *****
@@ -16,7 +17,7 @@ public class Solution_1 {
         sc.close();
 
         for (int i = 1; i <= rows; i++) {
-            for (int j = 1; j <= 2*rows-1 ; j++) {
+            for (int j = 1; j <= 2 * rows - 1; j++) {
                 if (j >= rows - (i - 1) && j <= rows + (i - 1)) {
                     System.out.print("*");
                 } else {
